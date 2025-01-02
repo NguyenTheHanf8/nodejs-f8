c3b3629d67f98682f13e6dae0375c7b476d6dd4a
NguyenTheHanf8 <nguyenthehan.dev@gmail.com>
2025-01-02 19:08:41 +0700

    Nộp btnv day02

--- index.js
+++ index.js
@@ -1,126 +1,106 @@
-import { createServer } from "node:http";
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/products";
 
-const hostname = "127.0.0.1";
+const app = express();
 const PORT = 8888;
-const uri = "http://localhost:3000";
-
-const fetchProducts = async () => {
-  const response = await fetch(`${uri}/products`);
-  const data = await response.json();
-  return data;
+const sendError = (res, status, message, data = null, error) => {
+  res.status(status).json({
+    message,
+    data,
+    error,
+  });
 };
 
-const server = createServer(async (req, res) => {
-  const url = req.url;
-  const method = req.method;
+app.use(express.json());
 
-  if (url === "/products" && method === "GET") {
-    const products = await fetchProducts();
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
+mongoose
+  .connect("mongodb://127.0.0.1:27017/my-db")
+  .then(() => {
+    console.log("Connect database successfully!");
+  })
+  .catch((error) => {
+    console.error(`Connect failed: ${error}`);
+  });
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    if (products.length === 0) {
+      return res.status(404).json({
+        message: "Không có sản phẩm nào",
+        list: [],
+      });
+    } else {
+      return res.status(200).json({
         message: "Lấy danh sách sản phẩm thành công",
         products,
-      })
-    );
-  } else if (url === "/products" && method === "POST") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", async () => {
-      const { title, price } = JSON.parse(body);
-      const newProduct = {
-        title,
-        price,
-      };
-
-      const response = await fetch(`${uri}/products`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newProduct),
       });
-
-      const createdProduct = await response.json();
-      res.writeHead(201);
-      res.end(JSON.stringify(createdProduct));
-    });
-  } else if (url.match(/\/products\/\d+/) && method === "PUT") {
-    const id = parseInt(url.split("/")[2]);
-
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", async () => {
-      const { title, price } = JSON.parse(body);
-      const updatedProduct = {
-        title,
-        price,
-      };
-
-      const response = await fetch(`${uri}/products/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedProduct),
-      });
-
-      if (response.ok) {
-        const product = await response.json();
-        res.writeHead(200);
-        res.end(JSON.stringify(product));
-      } else {
-        res.writeHead(404);
-        res.end(JSON.stringify({ message: "Không thấy sản phẩm" }));
-      }
-    });
-  } else if (url.match(/\/products\/\d+/) && method === "DELETE") {
-    const id = parseInt(url.split("/")[2]);
-
-    const response = await fetch(`${uri}/products/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      res.writeHead(200);
-      res.end(
-        JSON.stringify({
-          message: "Xóa sản phẩm thành công",
-        })
-      );
-    } else {
-      res.writeHead(404);
-      res.end(JSON.stringify({ message: "Không thấy sản phẩm" }));
     }
-  } else if (url.startsWith("/products/") && method === "GET") {
-    const id = parseInt(url.split("/")[2]);
-
-    const response = await fetch(`${uri}/products/${id}`);
-    if (response.ok) {
-      const product = await response.json();
-      res.end(
-        JSON.stringify({
-          message: "Lấy thông tin sản phẩm thành công",
-          product,
-        })
-      );
-    } else {
-      res.end(
-        JSON.stringify({
-          message: "Không thấy sản phẩm",
-        })
-      );
-    }
-  } else {
-    res.end("Router not found");
+  } catch (error) {
+    sendError(500, "Server Error", "Không thể lấy danh sách sản phẩm");
   }
 });
 
-server.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}/`);
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    } else {
+      return res.status(200).json({
+        message: "Lấy sản phẩm thành công",
+        product,
+      });
+    }
+  } catch (error) {
+    sendError(res, 500, "Server Error", "Không thể lấy sản phẩm");
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    }
+    res.status(200).json({ message: "Xóa thành công sản phẩm" });
+  } catch (error) {
+    sendError(res, 500, "Server Error", "Không thể xóa sản phẩm");
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json({
+      message: "Tạo sản phẩm thành công",
+      newProduct,
+    });
+  } catch (error) {
+    sendError(res, 500, "Server Error", "Không thể tạo sản phẩm");
+  }
+});
+
+app.patch("/products/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    }
+    res.status(200).json({
+      message: "Cập nhật sản phẩm thành công",
+      updatedProduct,
+    });
+  } catch (error) {
+    sendError(res, 500, "Server Error", "Không thể cập nhật sản phẩm");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
